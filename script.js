f2e017fc68ba44c4c683825d9fe4a619fe9f89d3
markosanchez800 <markosanchez800@gmail.com>
2021-04-02 22:29:53 -0700

    all input now saves to local storage and appears when page is reloaded

--- script.js
+++ script.js
@@ -9,18 +9,38 @@ var twoPM = document.getElementById("twoPM");
 var threePM = document.getElementById("threePM");
 var fourPM = document.getElementById("fourPM");
 var fivePM = document.getElementById("fivePM");
-var saveButton = document.getElementById("saveBtn");
+var saveyNine = $('#saveyNine');
+var saveyTen = $('#saveyTen');
+var saveyEleven = $('#saveyEleven');
+var saveyTwelve = $('#saveyTwelve');
+var saveyOne = $('#saveyOne');
+var saveyTwo = $('#saveyTwo');
+var saveyThree = $('#saveyThree');
+var saveyFour = $('#saveyFour');
+var saveyFive = $('#saveyFive');
+var storedNine = localStorage.getItem("firstTask");
+var storedTen = localStorage.getItem("secondTask");
+var storedEleven = localStorage.getItem("thirdTask");
+var storedTwelve = localStorage.getItem("fourthTask");
+var storedOne = localStorage.getItem("fifthTask");
+var storedTwo = localStorage.getItem("sixthTask");
+var storedThree = localStorage.getItem("seventhTask");
+var storedFour = localStorage.getItem("eigthTask");
+var storedFive = localStorage.getItem("ninthTask");
+
 console.log(currentHour);
 
 $("#currentDay").text(currentDate.format("LL"));
 
+
+renderTasks();
 
 timeColorCode(currentHour);
 function timeColorCode(currentHour){
     if (currentHour > 9){
         nineAM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 9){
+     else if (currentHour == 9){
         nineAM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 9){
         nineAM.setAttribute("class","col-sm-8 future");
@@ -28,7 +48,7 @@ function timeColorCode(currentHour){
     if (currentHour > 10){
         tenAM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 10){
+     else if (currentHour == 10){
         tenAM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 10){
         tenAM.setAttribute("class","col-sm-8 future");
@@ -36,7 +56,7 @@ function timeColorCode(currentHour){
     if (currentHour > 11){
         elevenAM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 11){
+     else if (currentHour == 11){
         elevenAM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 11){
         elevenAM.setAttribute("class","col-sm-8 future");
@@ -44,7 +64,7 @@ function timeColorCode(currentHour){
     if (currentHour > 12){
         twelvePM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 12){
+     else if (currentHour == 12){
         twelvePM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 12){
         twelvePM.setAttribute("class","col-sm-8 future");
@@ -52,7 +72,7 @@ function timeColorCode(currentHour){
     if (currentHour > 13){
         onePM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 13){
+     else if (currentHour == 13){
         onePM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 13){
         onePM.setAttribute("class","col-sm-8 future");
@@ -60,7 +80,7 @@ function timeColorCode(currentHour){
     if (currentHour > 14){
         twoPM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 14){
+     else if (currentHour == 14){
         twoPM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 14){
         twoPM.setAttribute("class","col-sm-8 future");
@@ -76,7 +96,7 @@ function timeColorCode(currentHour){
     if (currentHour > 16){
         fourPM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 16){
+     else if (currentHour == 16){
         fourPM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 16){
         fourPM.setAttribute("class","col-sm-8 future");
@@ -84,11 +104,57 @@ function timeColorCode(currentHour){
     if (currentHour > 17){
         fivePM.setAttribute("class","col-sm-8 past");
     }
-     else if (currentHour === 17){
+     else if (currentHour == 17){
         fivePM.setAttribute("class","col-sm-8 present");
     } else if (currentHour < 17){
         fivePM.setAttribute("class","col-sm-8 future");
     }
 }
 
+saveyNine.on('click', function(event){
+    localStorage.setItem("firstTask",JSON.stringify(nineAM.value));
+})
 
+saveyTen.on('click', function(event){
+    localStorage.setItem("secondTask",JSON.stringify(tenAM.value));
+})
+
+saveyEleven.on('click', function(event){
+    localStorage.setItem("thirdTask",JSON.stringify(elevenAM.value));
+})
+
+saveyTwelve.on('click', function(event){
+    localStorage.setItem("fourthTask",JSON.stringify(twelvePM.value));
+})
+
+saveyOne.on('click', function(event){
+    localStorage.setItem("fifthTask",JSON.stringify(onePM.value));
+})
+
+saveyTwo.on('click', function(event){
+    localStorage.setItem("sixthTask",JSON.stringify(twoPM.value));
+})
+
+saveyThree.on('click', function(event){
+    localStorage.setItem("seventhTask",JSON.stringify(threePM.value));
+})
+
+saveyFour.on('click', function(event){
+    localStorage.setItem("eigthTask",JSON.stringify(fourPM.value));
+})
+
+saveyFive.on('click', function(event){
+    localStorage.setItem("ninthTask",JSON.stringify(fivePM.value));
+})
+
+function renderTasks(){
+    nineAM.value = storedNine;
+    tenAM.value = storedTen;
+    elevenAM.value = storedEleven;
+    twelvePM.value = storedTwelve;
+    onePM.value = storedOne;
+    twoPM.value = storedTwo;
+    threePM.value = storedThree;
+    fourPM.value = storedFour;
+    fivePM.value = storedFive;
+}
